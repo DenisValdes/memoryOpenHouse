8fecc19c785064fa2e4b8694e530ab0ac89e4825
DennisVAldes <dennis.valdez@anima.edu.uy>
2019-10-10 10:37:52 -0300

    new file:   api/arrayCartas.js

---
+++ api/arrayCartas.js
@@ -0,0 +1,225 @@
+let cartas = [
+    {
+        id: 1,
+        foto:"",
+        texto:"Geogebra",
+        idPar: 1,
+        estado:"oculta"
+       
+    },
+    {
+        id: 2,
+        foto:"https://drive.google.com/open?id=10k3Ko64Pk6ESEU1ugaxXJeunlKLFjDJd",
+        texto:"",
+        idPar: 1,
+        estado:"oculta"
+       
+    },
+    {
+        id: 3,
+        foto:"",
+        texto:"Triangulo isosceles",
+        idPar: 2,
+        estado:"oculta"
+       
+    },
+    {
+        id: 4,
+        foto:"https://drive.google.com/open?id=1ptOVrBJiFIlVLpbS9JLC-9c24kwratzj",
+        texto:"",
+        idPar: 2,
+        estado:"oculta"
+       
+    },
+    {
+        id: 5,
+        foto:"",
+        texto:"Triángulo Equilátero",
+        idPar: 3,
+        estado:"oculta"
+       
+    },
+    {
+        id: 6,
+        foto:"https://drive.google.com/open?id=1HMv_gsk2Ndua_HZSrq1f3RSrZRCt0Wnq",
+        texto:"",
+        idPar: 3,
+        estado:"oculta"
+       
+    },
+    {
+        id: 7,
+        foto:"",
+        texto:"Triángulo Escaleno",
+        idPar: 4,
+        estado:"oculta"
+       
+    },
+    {
+        id: 8,
+        foto:"https://drive.google.com/open?id=1dePCekYIBKeSdvb2AFW57dDQ2Tge0HFe",
+        texto:"",
+        idPar: 4,
+        estado:"oculta"
+       
+    },
+    {
+        id: 9,
+        foto:"",
+        texto:"Arco Capaz",
+        idPar: 5,
+        estado:"oculta"
+       
+    },
+    {
+        id: 10,
+        foto:"https://drive.google.com/open?id=1zzaWenZrZ96qBmzqAFDW-SSbVBtpLADc",
+        texto:"",
+        idPar: 5,
+        estado:"oculta"
+        
+    },{
+        id: 11,
+        foto:"",
+        texto:"Homotecia",
+        idPar: 6,
+        estado:"oculta"
+        
+    },
+    {
+        id: 12,
+        foto:"https://drive.google.com/open?id=1d5ZBu13CIM_8LBTk-seDyK9P0nxsfu4w",
+        texto:"",
+        idPar: 6,
+        estado:"oculta"
+        
+    },
+    {
+        id: 13,
+        foto:"",
+        texto:"Dodecaedro",
+        idPar: 7,
+        estado:"oculta"
+        
+    },
+    {
+        id: 14,
+        foto:"https://drive.google.com/open?id=10g2J2ty1ANjcm4npOle3BKeGfu0qZV36",
+        texto:"",
+        idPar: 7,
+        estado:"oculta"
+        
+    },
+    {
+        id: 15,
+        foto:"",
+        texto:"Teorema de Pitágoras",
+        idPar: 8,
+        estado:"oculta"
+        
+    },
+    {
+        id: 16,
+        foto:"https://drive.google.com/open?id=1sUem9FUBRQcQPzZDuP1TMVxmprgrQi1K",
+        texto:"",
+        idPar: 8,
+        estado:"oculta"
+        
+    },
+    {
+        id: 17,
+        foto:"",
+        texto:"Pitágoras",
+        idPar: 9,
+        estado:"oculta"
+        
+    },
+    {
+        id: 18,
+        foto:"https://drive.google.com/open?id=1Eo5ZTPFgAksi15q04SUkRiBVuwrUV_6A",
+        texto:"",
+        idPar: 9,
+        estado:"oculta"
+        
+    },
+    {
+        id: 19,
+        foto:"",
+        texto:"Mediatriz",
+        idPar: 10,
+        estado:"oculta"
+        
+    },
+    {
+        id: 20,
+        foto:"https://drive.google.com/open?id=1S4S00qO9vuVP8vlNACdnkpA_YPtea9oH",
+        texto:"",
+        idPar: 10,
+        estado:"oculta"
+        
+    },
+    {
+        id: 21,
+        foto:"",
+        texto:"Bisectriz",
+        idPar: 11,
+        estado:"oculta"
+        
+    },
+    {
+        id: 22,
+        foto:"https://drive.google.com/open?id=1euJ09BidCQ2tDmE5SKW_X542RH3pryMG",
+        texto:"",
+        idPar: 11,
+        estado:"oculta"
+        
+    },
+    {
+        id: 23,
+        foto:"",
+        texto:"Ortocentro",
+        idPar: 12,
+        estado:"oculta"
+        
+    },
+    {
+        id: 24,
+        foto:"https://drive.google.com/open?id=1O49SSmCUkS28WslTIJmUAFirlIyM1cGj",
+        texto:"",
+        idPar: 12,
+        estado:"oculta"
+        
+    },
+    {
+        id: 25,
+        foto:"",
+        texto:"Paralelogramo tipo",
+        idPar: 13,
+        estado:"oculta"
+        
+    },
+    {
+        id: 26,
+        foto:"https://drive.google.com/open?id=1qaC5ctRygJxSs9ErOL_KtDovfbhb7BLD",
+        texto:"",
+        idPar: 13,
+        estado:"oculta"
+        
+    },
+    {
+        id: 27,
+        foto:"",
+        texto:"Tangente",
+        idPar: 14,
+        estado:"oculta"
+        
+    },
+    {
+        id: 28,
+        foto:"https://drive.google.com/open?id=19iKTrsqn8YNJhG5Vf405be1saBSkDXQa",
+        texto:"",
+        idPar: 14,
+        estado:"oculta"
+        
+    }
+]
