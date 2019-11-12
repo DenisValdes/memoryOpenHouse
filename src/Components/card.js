0823476cd1af0675bd964332eb5c83295adb0d64
DennisValdes <dennis@decemberlabs.com>
2019-11-12 09:48:43 -0300

    cosas

--- src/Components/card.js
+++ src/Components/card.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 import './Card.css'
 
+const gifs = [
+  "https://media.giphy.com/media/2WGYAQZ52I6wQecREk/giphy.gif",
+  "http://giphygifs.s3.amazonaws.com/media/SYrMAmJZT4YcU/giphy.gif",
+  "http://giphygifs.s3.amazonaws.com/media/ruCg20L6H72TK/giphy.gif",
+  "https://media.giphy.com/media/zsp6JrZQf3rPy/giphy.gif"
+];
+
+function shuffle(gifs) {
+  gifs.sort(() => Math.random() - 0.5);
+}
+
+shuffle(gifs);
+
 class Card extends React.Component {
   
   onClickCard = () => {
@@ -14,7 +27,9 @@ class Card extends React.Component {
 
     return (
               carta.estado === 'oculta' ?
-                <div onClick={this.onClickCard} className="card-hide"></div>
+                <div onClick={this.onClickCard} className="card-hide">
+                  <figure><img src={ gifs.map( function(num) { return Math.sqrt(num)} )} /></figure>
+                </div>
                 :
                 <div onClick={this.onClickCard} className="card-showed">
                   <img src={carta.foto} alt="" />
